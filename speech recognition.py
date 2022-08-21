a88f5f417b7925525435e70439e12c86be3f64ca
Aryaan Khettry <97103701+Aryaan-Khettry@users.noreply.github.com>
2022-08-21 13:01:38 +0530

    Update speech recognition.py

--- speech recognition.py
+++ speech recognition.py
@@ -1,35 +1,34 @@
-import speech_recognition as sr #To record the speech and convert to text
-import playsound #play the stored mp3 files
-from gtts import gTTS # google text to speech
+import speech_recognition as sr # To record the speech and convert to text
+import playsound # Play the stored mp3 files
+from gtts import gTTS # Google text to speech
 import os
-from selenium import webdriver # to control browser operations
+from selenium import webdriver # To control browser operations
 import io
 inc = 0
 name ='Human'
 def assistant_speaks(output):
-    global inc #To give each file a new name
+    # Create an mp3 file containing the assistants speech and play the speech
+    global inc # To give each file a new name
     inc += 1
     print("Assistant : "+ output)
-  
     toSpeak = gTTS(text = output, lang ='en') 
     file=str(inc)+".mp3"
     toSpeak.save(file)
-    playsound.playsound(file, True) #play the audio in the file
-    os.remove(file) #remove file after speech
+    playsound.playsound(file, True) 
+    os.remove(file) 
 
 def get_audio():
-  
+    # Storing user speech
     rObject = sr.Recognizer()
     audio=''
-    with sr.Microphone() as source: #Take user speech as input
+    with sr.Microphone() as source:
         print("Speak..")
-          
-        audio = rObject.listen(source, phrase_time_limit = 10) #user speech stored in audio
-    print("Processing...") #Waits for 10 seconds 
+        audio = rObject.listen(source, phrase_time_limit = 10)
+    print("Processing...") # Waits for 10 seconds for user input 
   
     try:
   
-        text = rObject.recognize_google(audio, language ='en-US') #Used to parse the users speech and place it in an array
+        text = rObject.recognize_google(audio, language ='en-US') # Parse the users speech
         print(name + " : ", text)
         return text
   
@@ -68,8 +67,9 @@ def process_text(input):
             search_web(input)
         else:
             return
+        
 def search_web(input):
-
+    
     driver = webdriver.Firefox()
     driver.implicitly_wait(1)
     driver.maximize_window()
@@ -113,7 +113,6 @@ def search_web(input):
 def open_application(input):
   
     if "chrome" in input or "google" in input:
-        assistant_speaks("Google Chrome")
         os.startfile('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
         return
   
@@ -138,7 +137,7 @@ def open_application(input):
         return
 
 if __name__ == "__main__":
-    assistant_speaks("What's your name, Human?")
+    assistant_speaks("Hello, I am your assistant. Please tell me your name.")
     name = get_audio()
     assistant_speaks("Hello, " + name + '.')
       
